b881928019c91cf49e62ae57457c991662c67757
Arlen Cornejo <arlen.rose.cr@gmail.com>
2019-09-02 19:10:24 -0700

    Updates

---
+++ precache-manifest.71eefa0e1fef6ffd531710435a90b313.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36082a96800e92e06466dc020ea95513",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "0bb8ba8304d05be1bd49",
+    "url": "/ClickyGame/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e2e9d5d734b08a2334cb",
+    "url": "/ClickyGame/static/js/2.c08360af.chunk.js"
+  },
+  {
+    "revision": "0bb8ba8304d05be1bd49",
+    "url": "/ClickyGame/static/js/main.4cb7c177.chunk.js"
+  },
+  {
+    "revision": "a459eb160378bf7375ad",
+    "url": "/ClickyGame/static/js/runtime~main.ce31df4d.js"
+  }
+]);
